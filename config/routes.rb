fde4bf8607b31e5ad3383b5b3afb8397c2d6a281
Jill Tankersley <jillian.emilie@gmail.com>
2020-01-04 16:44:31 -0500

    Set up webpacker

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,4 @@ Rails.application.routes.draw do
   resources :rooms
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'home#index'
 end
