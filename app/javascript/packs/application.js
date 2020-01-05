006dc28a8c7125776c7188b4465f1f103d0ceea3
Jill Tankersley <jillian.emilie@gmail.com>
2020-01-05 11:53:21 -0500

    Rip out material kit
    Worthless. Just worthless.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,16 +8,6 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
-// MaterialKit requirements https://dev.to/morinoko/adding-custom-javascript-in-rails-6-1ke6
-require('material-kit/core/jquery.min')
-require('material-kit/core/popper.min')
-require('material-kit/plugins/jquery')
-require('material-kit/plugins/nouislider.min')
-require('material-kit/core/bootstrap-material-design.min')
-require('material-kit/plugins/bootstrap-datetimepicker')
-require('material-kit/plugins/moment.min')
-require("material-kit/material-kit")
-
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
